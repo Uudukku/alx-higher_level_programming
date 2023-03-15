5cb5f13c7b333f1ec9aa4b5e5f22bbe2a22c900e
UmarMuktar <umarmuktarsiddi@gmail.com>
2023-03-15 05:37:10 -0700

    2-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+""" creates class Square """
+
+
 class Square:
+    """ Square class"""
     def __init__(self, size=0):
-        self.size = size
+        if type(size) != int and type(size) != float:
+            raise TypeError("size must be a number")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
@@ -9,29 +18,36 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        if type(value) != int and type(value) != float:
             raise TypeError("size must be a number")
-        if value < 0:
-            raise TypeError("size must be >= 0")
-        self.__size = value
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         return self.__size * self.__size
 
-    def __lt__(self, other):
-        return(self.area() < other.area())
-
     def __eq__(self, other):
+        """check if equal to another square"""
         return(self.area() == other.area())
 
+    def __lt__(self, other):
+        """check if less than other square"""
+        return(self.area() < other.area())
+
+    def __le__(self, other):
+        """check if less than or equal to other square"""
+        return(self.area() <= other.area())
+
     def __ne__(self, other):
+        """check if not equal to another suqare"""
         return(self.area() != other.area())
 
     def __gt__(self, other):
+        """check if greater than another square"""
         return(self.area() > other.area())
 
-    def __le__(self, other):
-        return(self.area() <= other.area())
-
     def __ge__(self, other):
+        """check if greater than or equal to another square"""
         return(self.area() >= other.area())
